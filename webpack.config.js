d00eb02cdcb5f293876f243df8ddd6f39925afc7
Hernandez Giordano <hernangior@gmail.com>
2023-10-12 10:03:17 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
-
 module.exports = {
+  mode: 'production',
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
   },
 };

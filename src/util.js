f9f016b8018ae8fcfc13e0cbbf60f3970a7824d8
Hernandez Giordano <hernangior@gmail.com>
2023-10-12 19:45:31 -0300

    Update util.js

--- src/util.js
+++ src/util.js
@@ -58,6 +58,56 @@ function printAttributes() {
 function test() {
     return __awaiter(this, void 0, void 0, function* () {
         let list = getCommitHistory('joaomarcus13', 'curso-angular');
+        let table = '';
+        let linha = '';
+        try {
+            const items = yield list;
+            for (const item of items) {
+                linha = linha + '<tr>';
+                const { sha, message, author, date } = item;
+                console.log('SHA:', sha);
+                linha = '<td>' + sha + '</td>';
+                console.log('Message:', message);
+                linha = '<td>' + message + '</td>';
+                console.log('Author:', author);
+                linha = '<td>' + author + '</td>';
+                console.log('Date:', date);
+                linha = '<td>' + date + '</td>';
+                console.log('---');
+                linha = linha + '</tr>';
+                table = table + linha;
+                linha = '';
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+        console.log('# ROTINA DE TESTE EXECUTANDO 18:50');
+        const divElement = document.getElementById('div-commits');
+        if (divElement) {
+            divElement.innerHTML = table;
+        }
         $('#test').modal('show');
     });
 }
+function loadCommitsFromBranch(owner, repo, branch) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield axios_1.default.get(`https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}`);
+            const commits = response.data;
+            // Mostra os commits no console
+            commits.forEach((commit) => {
+                const { sha, commit: { message, author: { name, email } } } = commit;
+                console.log('SHA:', sha);
+                console.log('Message:', message);
+                console.log('Author:', name);
+                console.log('Email:', email);
+                console.log('---');
+            });
+        }
+        catch (error) {
+            console.error('Error loading commits:', error);
+        }
+    });
+}
+loadCommitsFromBranch('joaomarcus13', 'curso-angular', 'main');

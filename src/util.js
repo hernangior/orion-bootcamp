de45d43b0fb717de5e6e82ebec974ced2de95105
Hernandez Giordano <hernangior@gmail.com>
2023-10-12 20:30:29 -0300

    Update util.js

--- src/util.js
+++ src/util.js
@@ -13,33 +13,129 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
-// Função para carregar commits e exibir na div HTML
-function loadCommits() {
+function getCommitHistory(owner, repo) {
     return __awaiter(this, void 0, void 0, function* () {
-        const owner = 'joaomarcus13';
-        const repo = 'curso-angular';
-        const branch = 'main';
+        try {
+            const response = yield axios_1.default.get(`https://api.github.com/repos/${owner}/${repo}/commits`);
+            //console.log(response);
+            if (response.status === 200) {
+                return response.data.map((commit) => ({
+                    sha: commit.sha,
+                    message: commit.commit.message,
+                    author: commit.commit.author.name,
+                    date: commit.commit.author.date,
+                }));
+            }
+            else {
+                throw new Error(`Failed to fetch commit history. Status code: ${response.status}`);
+            }
+        }
+        catch (error) {
+            throw new Error(`Error fetching commit history: ${error.message}`);
+        }
+    });
+}
+function printAttributes() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let list = getCommitHistory('joaomarcus13', 'curso-angular');
+        try {
+            const items = yield list;
+            for (const item of items) {
+                const { sha, message, author, date } = item;
+                console.log('SHA:', sha);
+                console.log('Message:', message);
+                console.log('Author:', author);
+                console.log('Date:', date);
+                console.log('---');
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+//printAttributes();
+function test() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let list = getCommitHistory('joaomarcus13', 'curso-angular');
+        let table = '';
+        let linha = '';
+        try {
+            const items = yield list;
+            for (const item of items) {
+                linha = linha + '<tr>';
+                const { sha, message, author, date } = item;
+                console.log('SHA:', sha);
+                linha = '<td>' + sha + '</td>';
+                console.log('Message:', message);
+                linha = '<td>' + message + '</td>';
+                console.log('Author:', author);
+                linha = '<td>' + author + '</td>';
+                console.log('Date:', date);
+                linha = '<td>' + date + '</td>';
+                console.log('---');
+                linha = linha + '</tr>';
+                table = table + linha;
+                linha = '';
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+        console.log('# ROTINA DE TESTE EXECUTANDO 18:50');
+        const divElement = document.getElementById('div-commits');
+        if (divElement) {
+            divElement.innerHTML = table;
+        }
+        $('#test').modal('show');
+    });
+}
+function loadCommitsFromBranch(owner, repo, branch) {
+    return __awaiter(this, void 0, void 0, function* () {
         try {
             const response = yield axios_1.default.get(`https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}`);
             const commits = response.data;
-            const commitsDiv = document.getElementById('commitsDiv');
-            if (commitsDiv) {
-                commitsDiv.innerHTML = ''; // Limpa o conteúdo atual da div
-                commits.forEach((commit) => {
-                    const { sha, commit: { message, author: { name, email } } } = commit;
-                    const commitInfo = `
+            // Mostra os commits no console
+            commits.forEach((commit) => {
+                const { sha, commit: { message, author: { name, email } } } = commit;
+                console.log('SHA:', sha);
+                console.log('Message:', message);
+                console.log('Author:', name);
+                console.log('Email:', email);
+                console.log('---');
+            });
+        }
+        catch (error) {
+            console.error('Error loading commits:', error);
+        }
+    });
+}
+//loadCommitsFromBranch('joaomarcus13', 'curso-angular','main');
+// Função para carregar commits e exibir na div HTML
+// Função para carregar commits e exibir na div HTML
+function loadCommits() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const owner = 'hernangior';
+        const repo = 'orion-bootcamp';
+        const branch = 'develop';
+        try {
+            const response = yield fetch(`https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}`);
+            const commits = yield response.json();
+            commits.forEach((commit) => {
+                const { sha, commit: { message, author: { name, email } } } = commit;
+                const commitInfo = `
           <p><strong>SHA:</strong> ${sha}</p>
           <p><strong>Message:</strong> ${message}</p>
           <p><strong>Author:</strong> ${name}</p>
           <p><strong>Email:</strong> ${email}</p>
           <hr>
         `;
-                    commitsDiv.innerHTML += commitInfo; // Adiciona as informações do commit à div
-                });
-            }
+                console.log(commitInfo);
+            });
         }
         catch (error) {
             console.error('Erro ao carregar os commits:', error);
         }
     });
 }
+loadCommits();

20af4506d7f3a441b67d54e1aab2c4ad256b4342
Hernandez Giordano <hernangior@gmail.com>
2023-10-12 10:51:43 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hernandez');
+// função que retorna a quantidade de vogais da palavra passada. 
+function getNumberOfVowels(word: string): number {
+    const regexExpression = /[aeiouàáâãäåæçèéêëìíîïðòóôõöùúûüýÿ]/gi;
+    const vowelsFound     = word.match(regexExpression);
+    return vowelsFound    ? vowelsFound.length : 0;
+}
+
+// a) Exemplo de uso com uma palavra recebida via parâmetro da função.
+console.log(getNumberOfVowels("Hernandez"));
+
+// b) Exemplo de uso com uma palavra recebida via input no formulário.
+function showNumberOfWowels() {
+    const inputWord      = document.getElementById('inputPalavra') as HTMLInputElement;
+    const numberOfVowels = getNumberOfVowels(inputWord.value);
+    alert('A quantidade de vogais da palavra informada é: ' + numberOfVowels);
+}

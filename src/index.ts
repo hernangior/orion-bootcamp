b877e4532a01f8de616b1a8bb7b63ee288301a89
Hernandez Giordano <hernangior@gmail.com>
2023-10-12 14:54:20 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,188 @@
-// Exercicio 01
+    /*
+    +
+    |
+    |   Exercicio 01
+    |
+    +
+    */ 
 
-// função que retorna a quantidade de vogais da palavra passada. 
-function getNumberOfVowels(word: string): number {
-    const regexExpression = /[aeiouàáâãäåæçèéêëìíîïðòóôõöùúûüýÿ]/gi;
-    const vowelsFound     = word.match(regexExpression);
-    return vowelsFound    ? vowelsFound.length : 0;
-}
+    // função que retorna a quantidade de vogais da palavra passada. 
+    function getNumberOfVowels(word: string): number {
+        let regexExpression = /[aeiouàáâãäåæçèéêëìíîïðòóôõöùúûüýÿ]/gi;
+        let vowelsFound     = word.match(regexExpression);
+        return vowelsFound    ? vowelsFound.length : 0;
+    }
 
-// a) Exemplo de uso com uma palavra recebida via parâmetro da função.
-console.log(getNumberOfVowels("Hernandez"));
+    // a) Exemplo de uso com uma palavra recebida via parâmetro da função.
+    /*
+    let word = "Hernandez";
+    console.log("# Na palavra ["+word+"] temos ["+getNumberOfVowels(word)+"] vogais");
+    */
 
-// b) Exemplo de uso com uma palavra recebida via input no formulário.
-function showNumberOfWowels() {
-    const inputWord      = document.getElementById('inputPalavra') as HTMLInputElement;
-    const numberOfVowels = getNumberOfVowels(inputWord.value);
-    alert('A quantidade de vogais da palavra informada é: ' + numberOfVowels);
-}
+    // b) Exemplo de uso com uma palavra recebida via input no formulário.
+    function showNumberOfWowels() {
+        let inputWord      = document.getElementById('inputPalavra') as HTMLInputElement;
+        let numberOfVowels = getNumberOfVowels(inputWord.value);
+        alert('A quantidade de vogais da palavra informada é: ' + numberOfVowels);
+    }
 
+    /*
+    +
+    |
+    |   Exercicio 02
+    |
+    +
+    */ 
 
+    let lista : Array <Object> = [{
+        "id": 1,
+        "name": "Ada Lovelace",
+        "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"
+    },
+    {
+        "id": 2,
+        "name": "Alan Turing",
+        "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial"
+    },
+    {
+        "id": 3,
+        "name": "Nikola Tesla",
+        "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."
+    },
+    {
+        "id": 4,
+        "name": "Nicolau Copérnico",
+        "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."
+    }
+    ];
+
+    //console.log("# Lista de dados em array para Exercício 02:");
+    //console.table(lista);
+
+    class Person {
+        id  : number;
+        name: string;
+        bio : string;
+        constructor(id: number, name: string, bio: string) {
+            this.id   = id;
+            this.name = name;
+            this.bio  = bio;
+        }
+    }
+
+    function convertArrayToList(array: Array<any>): Array<Person> {
+        return array.map(
+                            (obj) => new Person(obj.id, obj.name, obj.bio)
+                        );
+    }
+
+    let newList = convertArrayToList(lista);
+
+    //console.log("# Lista de objetos para Exercício 02:");
+    //console.log(newList);
+
+    // a) Crie uma função que retorne a bio do id passado
+    // usando [paradigma funcional]
+    function functionalGetBioById(id: number): string {
+        let person = newList.find((item) => item.id === id);
+        return (person) ?  person.bio : '';
+    }
+    //console.log(functionalGetBioById(1));
+
+    // usando [paradigma imperativo]
+    function imperativeGetBioById(id: number): string{
+        let result = '';
+        for (let i = 0; i < lista.length; i++) {
+            let item = lista[i] as 
+                { 
+                    id  : number, 
+                    name: string, 
+                    bio : string 
+                }; 
+            if (item.id === id) {
+                result = item.bio;
+            }
+        }
+        return result;
+    }
+    //console.log(imperativeGetBioById(1));
+
+    // b) Crie uma função que retorne o name do id passado
+    // usando [paradigma funcional]
+    function functionalGetNameById(id: number): string {
+        let person = newList.find((item) => item.id === id);
+        return (person) ?  person.name : '';
+    }
+    //console.log(functionalGetNameById(1));
+
+    // usando [paradigma imperativo]
+    function imperativeGetNameById(id: number): string{
+        let result = '';
+        for (let i = 0; i < lista.length; i++) {
+            let item = lista[i] as 
+                { 
+                    id  : number, 
+                    name: string, 
+                    bio : string 
+                }; 
+            if (item.id === id) {
+                result = item.name;
+            }
+        }
+        return result;
+    }
+    //console.log(imperativeGetNameById(1));
+
+    // c) Crie uma função que apague um item da lista a partir de um id passado
+    // usando [paradigma funcional]
+    function functionalDeleteItemById(id: number): void {
+        let index = newList.findIndex((item) => item.id === id);
+        (index !== -1) ? newList.splice(index, 1): '';
+    }
+    //functionalDeleteItemById(2);
+    //console.table(newList);
+
+    // usando [paradigma imperativo]
+    function imperativeDeleteItemById(id: number): void {
+        for (let i = 0; i < newList.length; i++) {
+          if (newList[i].id === id) {
+            newList.splice(i, 1);
+            break;
+          }
+        }
+      }
+    //imperativeDeleteItemById(1);
+    //console.table(newList);
+
+    // d) Crie uma função que altere a bio ou o name a partir de um id passado
+    // usando [paradigma funcional]
+    function functionalUpdateItemById(id: number, newContent: string, type: string): void {
+        let item = newList.find((item) => item.id === id);
+        if (item){
+          (type == "bio") ? item.bio  = newContent:'';
+          (type == "name")? item.name = newContent:'';
+        }
+    }
+    //functionalUpdateItemById(1,"Aluno de Bootcamp","bio");
+    //functionalUpdateItemById(1,"Hernandez","name");
+    //console.table(newList);
+
+    // usando [paradigma imperativo]
+    function imperativeUpdateItemById(id: number, newContent: string, type: string): void {
+        for (let i = 0; i < newList.length; i++) {
+          if (newList[i].id === id) {
+            if (type == "bio") {
+              newList[i].bio = newContent;
+            }
+            if (type == "name") {
+              newList[i].name = newContent;
+            }
+            break;
+          }
+        }
+    }
+    //imperativeUpdateItemById(2,"Aluno de Bootcamp","bio");
+    //imperativeUpdateItemById(2,"Hernandez","name");
+    //console.table(newList);
+
+    // e) Demonstre todas as funções com o paradigma funcional e com o imperativo

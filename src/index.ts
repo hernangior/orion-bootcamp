94d9b7dabb887fb0762eb02a4460b5251223f7a7
Hernandez Giordano <hernangior@gmail.com>
2023-10-13 16:41:50 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-// função que retorna a quantidade de vogais da palavra passada. 
+  /**
+   * Função que retorna a quantidade de vogais de uma palavra informada
+   *
+   *
+   * @param word - A palavra que se deseja contar a quantidade de vogais
+   * @returns A quantidade de vogais de uma palavra informada
+   *
+   * @beta
+   */
 function getNumberOfVowels(word: string): number {
     const regexExpression = /[aeiouàáâãäåæçèéêëìíîïðòóôõöùúûüýÿ]/gi;
     const vowelsFound     = word.match(regexExpression);
@@ -9,8 +17,9 @@ function getNumberOfVowels(word: string): number {
 console.log(getNumberOfVowels("Hernandez"));
 
 // b) Exemplo de uso com uma palavra recebida via input no formulário.
-function showNumberOfWowels() {
+function showNumberOfVowels() : void {
     const inputWord      = document.getElementById('inputPalavra') as HTMLInputElement;
     const numberOfVowels = getNumberOfVowels(inputWord.value);
-    alert('A quantidade de vogais da palavra informada é: ' + numberOfVowels);
+    let divOutput = document.getElementById('div-output');
+    (divOutput)? divOutput.innerHTML = `# A quantidade de vogais é: ${numberOfVowels.toString()}`:'erro ao calcular';
 }

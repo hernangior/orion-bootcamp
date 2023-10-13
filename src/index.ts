22ee04bd6a3df98d4a03351a0f93e7e96ae9d1cd
Hernandez Giordano <hernangior@gmail.com>
2023-10-13 17:30:15 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-  /**
-   * Função que retorna a quantidade de vogais de uma palavra informada
-   *
-   *
-   * @param word - A palavra que se deseja contar a quantidade de vogais
-   * @returns A quantidade de vogais de uma palavra informada
-   *
-   * @beta
-   */
+/**
+ * Função que retorna a quantidade de vogais de uma palavra informada
+ *
+ *
+ * @param word - A palavra que se deseja contar a quantidade de vogais
+ * @returns A quantidade de vogais de uma palavra informada
+ *
+ * @beta
+ */
 function getNumberOfVowels(word: string): number {
     const regexExpression = /[aeiouàáâãäåæçèéêëìíîïðòóôõöùúûüýÿ]/gi;
-    const vowelsFound     = word.toLowerCase().match(regexExpression);
-    return vowelsFound    ? vowelsFound.length : 0;
-}
-
-// a) Exemplo de uso com uma palavra recebida via parâmetro da função.
-console.log("# função para contagem de vogais de uma palavra dada");
-console.log('# teste executado par a palavra: "Hernandez"');
-console.log(`A quantidade de vogais da palavra informada é: [${getNumberOfVowels("Hernandez")}]`);
-
-// b) Exemplo de uso com uma palavra recebida via input no formulário.
-function showNumberOfVowels(): void {
-    const inputWord = document.getElementById("inputPalavra") as HTMLInputElement;
+    const vowelsFound = word.toLowerCase().match(regexExpression);
+    return vowelsFound ? vowelsFound.length : 0;
+  }
+  
+  // a) Exemplo de uso com uma palavra recebida via parâmetro da função.
+  console.log("# função para contagem de vogais de uma palavra dada");
+  console.log('# teste executado par a palavra: "Hernandez"');
+  console.log(
+    `A quantidade de vogais da palavra informada é: [${getNumberOfVowels(
+      "Hernandez"
+    )}]`
+  );
+  
+  // b) Exemplo de uso com uma palavra recebida via input no formulário.
+  function showNumberOfVowels(): void {
+    const inputWord = document.getElementById("input-task-1") as HTMLInputElement;
     const numberOfVowels = getNumberOfVowels(inputWord.value);
     let divOutput = document.getElementById("div-output");
     if (divOutput) {

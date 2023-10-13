001caa4d2cffafcf9e46e0afbe522e6853226ae3
Hernandez  Giordano B de Sena Oliveira <hernangior@gmail.com>
2023-10-13 12:00:23 -0300

    Update util.js

--- src/util.js
+++ src/util.js
@@ -13,11 +13,14 @@ function loadCommits(branch) {
     return __awaiter(this, void 0, void 0, function* () {
         const owner = 'hernangior';
         const repo = 'orion-bootcamp';
-        const perPage = 1000;
+        const perPage = 100;
         try {
-            const response = yield fetch(`https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}&per_page=${perPage}`);
+            const response = yield fetch(`https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}&per_page=${perPage}&page=${1}`);
             let commits = yield response.json();
+            //if (commits.length <= 100){
             commits = commits.reverse();
+            //}
+            //
             let commitInfo = '';
             let order = 1;
             commits.forEach((commit) => {
@@ -33,20 +36,48 @@ function loadCommits(branch) {
         `;
                 order++;
             });
-            console.log(commitInfo);
+            //console.log(commitInfo);
             const divElement = document.getElementById('table-body-task-01');
             const divBranchName = document.getElementById('div-banch-name');
             const divBranchCount = document.getElementById('div-banch-count');
-            if (divElement && divBranchName && divBranchCount) {
-                divElement.innerHTML = commitInfo;
-                divBranchName.innerHTML = `
+            if (commits.length <= 100) {
+                if (divElement && divBranchName && divBranchCount) {
+                    divElement.innerHTML = commitInfo;
+                    divBranchName.innerHTML = `
         <i class="fas fa-code-branch fa-lg"></i>&nbsp&nbspHistórico de commits - <b>branch [${branch}]</b>
         `;
-                divBranchCount.innerHTML = `
+                    divBranchCount.innerHTML = `
         <i class="fas fa-list-ol fa-lg"></i>&nbsp&nbspQuantidade de commits realizados - <b>[${commits.length}]</b>
         `;
-                $('#test').modal('show');
+                }
             }
+            else {
+                const response2 = yield fetch(`https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}&per_page=${perPage}&page=${2}`);
+                let commits2 = (yield response2.json()).reverse();
+                let commitInfo2 = '';
+                //console.log(`A pagina 2 tem tamanho de [${commits2.length}] elementos`);
+                commits2.forEach((commit) => {
+                    const { sha, commit: { message, author: { name, email, date } }, } = commit;
+                    commitInfo2 += `
+            <tr>
+            <td>${order}</td>
+            <td>${date}</td>
+            <td>${name}</td>
+            <td>${message}</td>
+            <td>${sha}</td>
+            <tr>
+          `;
+                    order++;
+                });
+                commitInfo2 += commitInfo;
+                if (divElement && divBranchName && divBranchCount) {
+                    divElement.innerHTML = commitInfo2;
+                    divBranchCount.innerHTML = `
+        <i class="fas fa-list-ol fa-lg"></i>&nbsp&nbspQuantidade de commits realizados - <b>[${commits.length + commits2.length}]</b>
+        `;
+                }
+            }
+            $('#test').modal('show');
         }
         catch (error) {
             console.error('Erro ao carregar os commits:', error);
